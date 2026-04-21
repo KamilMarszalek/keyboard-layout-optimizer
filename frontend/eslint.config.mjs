a6a1facf4971a5d391b9e69bf7e07c9f584376df
KamilMarszalek <kamilmar553@gmail.com>
2026-04-21 17:04:41 +0200

    chore: add js lint

---
+++ frontend/eslint.config.mjs
@@ -0,0 +1,46 @@
+import js from '@eslint/js'
+import pluginVue from 'eslint-plugin-vue'
+import { defineConfig } from 'eslint/config'
+import globals from 'globals'
+import tseslint from 'typescript-eslint'
+
+export default defineConfig([
+  {
+    ignores: ['dist/**', 'coverage/**', 'node_modules/**'],
+  },
+
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
+  ...pluginVue.configs['flat/recommended'],
+
+  {
+    files: ['**/*.{ts,vue}'],
+    languageOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+      globals: globals.browser,
+      parserOptions: {
+        parser: tseslint.parser,
+      },
+    },
+    rules: {
+      'no-console': 'warn',
+      'prefer-const': 'error',
+
+      'vue/multi-word-component-names': 'off',
+      'vue/html-self-closing': ['error', {
+        html: {
+          void: 'always',
+          normal: 'never',
+          component: 'always',
+        },
+        svg: 'always',
+        math: 'always',
+      }],
+      'vue/max-attributes-per-line': ['warn', {
+        singleline: 3,
+        multiline: 1,
+      }],
+    },
+  },
+])
