197a8897550c12247b63eb47e7c26813cd4d5398
Michał Szwejk <mszwejk1@gmail.com>
2026-04-26 22:53:05 +0200

    test: add tests for modifiers

--- optimizer/src/keyboard/modifier.rs
+++ optimizer/src/keyboard/modifier.rs
@@ -74,3 +74,42 @@ impl Modifier {
         Self::new(letter_pairs.chain(punctuation_pairs))
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::keyboard::{common::KEY_COUNT, modifier};
+
+    #[test]
+    fn shift_supported() {
+        let modifier = Modifier::new([(b'a', b'A'), (b'z', b'Z'), (b'1', b'!'), (b'/', b'?')]);
+        assert_eq!(modifier.shift(b'a').unwrap(), b'A');
+        assert_eq!(modifier.shift(b'z').unwrap(), b'Z');
+        assert_eq!(modifier.shift(b'1').unwrap(), b'!');
+        assert_eq!(modifier.shift(b'/').unwrap(), b'?');
+    }
+
+    #[test]
+    fn shift_unsupported() {
+        let modifier = Modifier::new([(b'a', b'A'), (b'1', b'!')]);
+        assert!(matches!(modifier.shift(b'B'), Err(ModifierError::UnsupportedBase(b'B'))));
+        assert!(matches!(modifier.shift(b'/'), Err(ModifierError::UnsupportedBase(b'/'))));
+    }
+
+    #[test]
+    fn base_symbols() {
+        let modifier = Modifier::new([(b'a', b'A'), (b'1', b'!')]);
+        let mut symbols: Vec<AsciiChar> = modifier.base_symbols().to_vec();
+        symbols.sort();
+        assert_eq!(symbols, [b'1', b'a']);
+    }
+
+    #[test]
+    fn standard_us() {
+        let modifier = Modifier::standard_us();
+        assert_eq!(modifier.base_symbols().len(), KEY_COUNT);
+        for &symbol in modifier.base_symbols().iter() {
+            modifier.shift(symbol).unwrap();
+        }
+    }
+}
