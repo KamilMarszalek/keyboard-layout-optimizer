d92125a1c08643e4a2c8a04b887654d0745362c8
KamilMarszalek <kamilmar553@gmail.com>
2026-04-27 19:00:16 +0200

    fix: update normalize_text to transliterate and filter only graphic chars

--- optimizer/src/text/normalize.rs
+++ optimizer/src/text/normalize.rs
@@ -1,5 +1,7 @@
+use any_ascii::any_ascii;
+
 pub fn normalize_text(input: &str) -> String {
-    input.chars().filter(|c| c.is_ascii_alphabetic()).map(|c| c.to_ascii_lowercase()).collect()
+    any_ascii(input).chars().filter(|c| c.is_ascii_graphic()).collect()
 }
 
 #[cfg(test)]
@@ -8,7 +10,7 @@ mod tests {
 
     #[test]
     fn normalize_text_keeps_only_ascii_letters_and_lowercases() {
-        let result = normalize_text("Ala ma Kota! 123");
-        assert_eq!(result, "alamakota");
+        let result = normalize_text("Ąłą ma Kota! 123");
+        assert_eq!(result, "AlamaKota!123");
     }
 }
