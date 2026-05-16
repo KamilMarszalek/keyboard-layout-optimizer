47b12467a2b9f3a280c29395d95f64b22f2d0abc
KamilMarszalek <kamilmar553@gmail.com>
2026-05-16 16:30:28 +0200

    refactor: rename KEY_COUNT and add US_PRESS_COUNT

--- optimizer/src/keyboard/common.rs
+++ optimizer/src/keyboard/common.rs
@@ -1,4 +1,6 @@
-pub const KEY_COUNT: usize = 26 + 10 + 11; // (letters + digits + punctuation);
+pub const US_KEY_COUNT: usize = 26 + 10 + 11; // letters + digits + punctuation
+pub const US_PRESS_COUNT: usize = 2 * US_KEY_COUNT;
+
 pub const ASCII_COUNT: usize = 128;
 pub type AsciiChar = u8;
 pub type KeyIndex = usize;
