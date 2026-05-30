4345d1808dd387d0ac765dc8b34fa12b2afa09fd
KamilMarszalek <kamilmar553@gmail.com>
2026-05-30 21:49:24 +0200

    refactor: extract corpus.store.ts

---
+++ frontend/src/features/corpus/corpus.store.ts
@@ -0,0 +1,17 @@
+import { defineStore } from 'pinia';
+import { defaultText } from './corpus.schema';
+
+interface CorpusState {
+  text: string;
+}
+
+export const useCorpusStore = defineStore('corpus', {
+  state: (): CorpusState => ({
+    text: defaultText,
+  }),
+  actions: {
+    setText(text: string) {
+      this.text = text;
+    },
+  },
+});
