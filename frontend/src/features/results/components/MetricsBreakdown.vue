e6e6b4a1dedbecbf8a3746d70600e8b9fb3a50b8
Michał Szwejk <mszwejk1@gmail.com>
2026-06-06 14:15:43 +0200

    style: change constants to controls

--- frontend/src/features/results/components/MetricsBreakdown.vue
+++ frontend/src/features/results/components/MetricsBreakdown.vue
@@ -3,7 +3,7 @@ import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
 import { storeToRefs } from 'pinia';
 import { computed } from 'vue';
 
-import { metricLabels } from '../results.constants';
+import { metricLabels } from '../results.controls';
 import { useResultsStore } from '../results.store';
 
 const store = useResultsStore();
