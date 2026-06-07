012637ac6e117e0baa737df935adec9592e623bc
Michał Szwejk <mszwejk1@gmail.com>
2026-06-07 16:36:48 +0200

    feat: disable mode toggle when optimizer is runnig

--- frontend/src/features/mode/components/ModeToggle.vue
+++ frontend/src/features/mode/components/ModeToggle.vue
@@ -1,11 +1,15 @@
 <script setup lang="ts">
 import { Button } from '@/components/ui/button';
+import { useOptimizerStore } from '@/features/optimizer/store';
 import { storeToRefs } from 'pinia';
 
 import { useModeStore } from '../store';
 
-const store = useModeStore();
-const { mode } = storeToRefs(store);
+const modeStore = useModeStore();
+const { mode } = storeToRefs(modeStore);
+
+const optimizerStore = useOptimizerStore();
+const { isOptimizing } = storeToRefs(optimizerStore);
 </script>
 
 <template>
@@ -15,18 +19,20 @@ const { mode } = storeToRefs(store);
         type="button"
         size="sm"
         :variant="mode === 'optimize' ? 'default' : 'ghost'"
-        @click="store.setMode('optimize')"
+        @click="modeStore.setMode('optimize')"
       >
         Optimize
       </Button>
-      <Button
-        type="button"
-        size="sm"
-        :variant="mode === 'evaluate' ? 'default' : 'ghost'"
-        @click="store.setMode('evaluate')"
-      >
-        Evaluate
-      </Button>
+      <div :class="isOptimizing ? 'opacity-50 pointer-events-none select-none' : ''">
+        <Button
+          type="button"
+          size="sm"
+          :variant="mode === 'evaluate' ? 'default' : 'ghost'"
+          @click="modeStore.setMode('evaluate')"
+        >
+          Evaluate
+        </Button>
+      </div>
     </div>
   </div>
 </template>
